31b391baa2f0b0b5e2ff09a6147574c4ffb50c0a
El imali hamza <elimal.hamza@gmail.com>
2024-04-27 19:32:22 +0000

    .

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- script that lists all bands with Glam rock.
 SET @this_year := 2022;
 
-SELECT band_name, IF(split IS NULL, @this_year, split) - formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%'
+SELECT band_name, IF(split IS NULL, 2022, split) - formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
